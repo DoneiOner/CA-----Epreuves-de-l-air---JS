e9978a3db6e41433088f75b605c6e4ff27c10e96
donei <donei.dev@gmail.com>
2022-03-09 08:04:06 +0100

    exo 06

--- air06.js
+++ air06.js
@@ -12,3 +12,47 @@ $> python exo.py “Michel” “Albert” “Thérèse” “Benoit” “a”
 Michel, Thérèse, Benoit
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 */
+
+// pseudo-code :
+// - mettre les arguments dans un tableau
+// - let discriminant = dernier element du tableau
+// - parcourir le tableau, pour chaque element verifier si la chaine "discriminant" est presente dans la chaine parcourue => element.includes(discriminant) (renvoie true ou false)
+//          - si oui : continue
+//          - si non : push dans le nouveau tableau
+// - afficher le nouveau tableau
+
+// GESTION D'ERREURS :
+if (process.argv[3] === undefined) {
+	console.log("Erreur.");
+	process.exit(0);
+}
+
+
+// PARSING : 
+let array = [];
+for (let index = 2; index < process.argv.length; index++) {
+	array.push(process.argv[index]);
+	//console.log(array);	
+};
+
+let discriminant = array[array.length-1];
+//console.log(discriminant);
+
+let array2 = [];
+
+// RESOLUTION : 
+function eraseDoublons (array, discriminant) {
+for (let index = 0; index < array.length-1; index++) {
+	if (!array[index].includes(discriminant)) {
+		array2.push(array[index]);	
+}
+};
+};
+//console.log(array2);
+eraseDoublons(array, discriminant);
+
+// AFFICHAGE :
+for (let index = 0; index < array2.length; index++) {
+	process.stdout.write(`${array2[index]}` + ' ');
+}
+console.log(' ');
